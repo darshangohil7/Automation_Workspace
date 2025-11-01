b942f624513499ae3c82ce675479ba45e20e6589
Darshan Gohil <gohildarshan151@gmail.com>
2025-11-01 11:03:57 +0530

    Add files via upload

---
+++ Module - 6/Program_6_Fibonacci_Series.java
@@ -0,0 +1,25 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class Program_6_Fibonacci_Series {
+
+	public static void main(String[] args) 
+	{
+		
+		//....Fibonacci Series....//
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Any number to Get Fibonacci Series:");
+		int a = sc.nextInt();
+		int f=0, s=1, n;
+		for(int i=1;i<=a;i++) 
+		{
+			System.out.print(f+"  ");
+			n=f+s;
+			f=s;
+			s=n;
+		}
+
+	}
+
+}
